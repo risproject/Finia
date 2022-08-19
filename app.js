4b6c6cfdc9d2dc3c434c5ba24a580bb68a3bc385
risproject <faaris.faiz.fadlan@gmail.com>
2022-08-19 14:01:06 +0700

    update 0.2

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-        const save = document.querySelector('#save');
-        let tema = localStorage.getItem('tema');
-
-        if(!tema){
-            localStorage.setItem('tema', 'light');
-        }
-
-        const darkMode = () => {
-            document.body.classList.add('dark-mode');
-            toggle.innerHTML = 'light mode';
-            localStorage.setItem('tema', 'dark');
-        }
-        const lightMode = () => {
-            document.body.classList.remove('dark-mode');
-            toggle.innerHTML = 'light mode';
-            localStorage.setItem('tema', 'light');
-        }
-
-        if(tema === 'dark'){
-            darkMode();
-        }
-
-        save.addEventListener('click', function() {
-            tema = localStorage.getItem('tema');
-            if(tema === 'light') {
-                darkMode();
-            } else {
-                lightMode();
+async function getData() {
+    const spreadsheetId = '1c8dhJRb-FDS4lhppO7VCTdAc_KcWEOhkBJ-e0BatTgE'
+    const apiKey = 'AIzaSyCcCIXoql9YOJSmf-B8o9miTBTdxVtvzU8';
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/?key=${apiKey}&includeGridData=true`;
+    const result = await fetch(url)
+    const { sheets } = await result.json();
+    const eventSheet = sheets[0];
+    const data = eventSheet.data[0].rowData
+        .filter((_, index) => index !== 0) // Mulai dari index 1 (menghindari nama kolom)
+        .map(row => {
+            const { values } = row;
+            return {
+                name: values[0].formattedValue,
+                email: values[1].formattedValue,
             }
-
-            e.preventDefault();
-        });
+        })
+    return data;
+  }
+  
+  function dataItemTemplate(item) {
+    return (
+      `<li>
+        <p>${item.name}</p>
+        <p>${item.email}</p>
+      </li>`
+    )
+  }
+  
+  async function renderData() {
+    const wrapperDOM = document.getElementById('wrapper');
+    try {
+      const data = await getData();
+      wrapperDOM.innerHTML = data.map(item => dataItemTemplate(item)).join('');
+    } catch (error) {
+      wrapperDOM.innerHTML = error
+    }
+  }
+  
+  renderData();
